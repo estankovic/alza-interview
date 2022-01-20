9603e500acb7b2368df68d6af47772838c465c30
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2022-01-20 15:18:52 +0100

    Added Basic Logic for Hero List

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes.component';
 
-const routes: Routes = [{ path: '', component: HeroesComponent }];
+const routes: Routes = [
+  { path: '', component: HeroesComponent },
+  { path: ':id', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {HeroesService} from '../heroes-data/heroes.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {Hero} from '../heroes-data/hero.types';
+import {map, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroesComponent implements OnInit {
 
   readonly heroes$ = this.heroService.getHeroes$();
 
-  constructor(private readonly heroService: HeroesService) { }
+  constructor(
+    private readonly heroService: HeroesService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  getActiveHero$(): Observable<Hero | undefined> {
+    return this.route.paramMap.pipe(
+      map(params => parseInt(params.get('id') || '', 10)),
+      switchMap(id => this.heroService.getHero$(id))
+    )
+  }
+
+  onHeroClick(hero: Hero) {
+    this.router.navigate(['heroes', hero.id]);
+  }
+
+  onDetailsClick(hero: Hero) {
+    this.router.navigate(['heroes', hero.id, 'edit']);
   }
 
 }
