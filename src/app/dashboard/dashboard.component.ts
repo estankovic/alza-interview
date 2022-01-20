9a69c5c4a9fbf8d0b150c271c42270defa93a4c4
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2022-01-20 15:08:15 +0100

    Added Basic Skeleton for Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroesService} from '../heroes-data/heroes.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  readonly heroes$ = this.heroService.getTopHeroes$();
+
+  constructor(readonly heroService: HeroesService) { }
 
   ngOnInit(): void {
   }
