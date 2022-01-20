e5ebf6e9105d61c9d759c563aba42946c0661e7b
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2022-01-20 14:45:51 +0100

    Added data logic for heroes

---
+++ src/app/heroes-data/hero.data.ts
@@ -0,0 +1,21 @@
+import {Hero} from './hero.types';
+
+export const data: Hero[] = [
+  {
+    id: 1,
+    name: 'Amazing Spider-man'
+  },
+  {
+    id: 2,
+    name: 'Magneto'
+  },
+  {
+    id: 3,
+    name: 'Batman'
+  },
+  {
+    id: 4,
+    name: 'Storm'
+  }
+];
+
---
+++ src/app/heroes-data/heroes.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {Hero} from './hero.types';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {data} from './hero.data';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private heroesSubject$ = new BehaviorSubject<Hero[]>(data);
+
+  heroes$ = this.heroesSubject$.asObservable();
+
+  constructor() { }
+
+  getHeroes$(): Observable<Hero[]> {
+    return this.heroes$;
+  }
+
+  updateHero(hero: Hero) {
+    const heroes = [...this.heroesSubject$.getValue()];
+    const heroIndex = heroes.findIndex(h => hero.id === h.id);
+    heroes.splice(heroIndex, 1, hero);
+
+    this.heroesSubject$.next(heroes);
+  }
+
+  removeHero(id: number) {
+    const heroes = [...this.heroesSubject$.getValue()];
+    const heroIndex = heroes.findIndex(h => id === h.id);
+    heroes.splice(heroIndex, 1);
+
+    this.heroesSubject$.next(heroes);
+  }
+
+  getHero$(id: number): Observable<Hero | undefined> {
+    return this.getHeroes$().pipe(
+      map(heroes => heroes.find(hero => hero.id === id))
+    );
+  }
+
+}
